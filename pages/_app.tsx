fe47c6e9edf75fa0dc4ec57ff9eb8852c6025ba3
denis_levchenko <tranzi90@gmail.com>
2022-11-22 23:03:04 +0600

    fix content width

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,30 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
-import store, {persistor} from '../store'
+import store, { persistor } from '../store'
 import { Provider } from 'react-redux'
-import Head from "next/head"
-import Sidebar from "../components/Sidebar"
-import {PersistGate} from "redux-persist/integration/react"
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+import { PersistGate } from 'redux-persist/integration/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-      <>
-          <Head>
-              <title>Assessment test</title>
-              <link rel="icon" type="image/svg" href="/icon.svg" />
-          </Head>
-          <Provider store={store}>
-              <PersistGate persistor={persistor} loading={null}>
-                  <div className="flex">
-                      <Sidebar />
-                      <div className="w-full">
-                          <Component {...pageProps} />
-                      </div>
-                  </div>
-              </PersistGate>
-          </Provider>
-      </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Assessment test</title>
+                <link rel="icon" type="image/svg" href="/icon.svg" />
+            </Head>
+            <Provider store={store}>
+                <PersistGate persistor={persistor} loading={null}>
+                    <div className="flex">
+                        <Sidebar />
+                        <div className="w-full md:w-[calc(100%-208px)]">
+                            <Component {...pageProps} />
+                        </div>
+                    </div>
+                </PersistGate>
+            </Provider>
+        </>
+    )
 }
 
 export default MyApp
